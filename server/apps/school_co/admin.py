4a1ddf43e6c6fde9566f9e10c97c63db164c56ee
Vladimir <vmstarchenko@edu.hse.ru>
2021-10-11 13:28:47 +0300

    add models for school_co app

--- server/apps/school_co/admin.py
+++ server/apps/school_co/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
 # Register your models here.
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import School, Pupil, LearnerTextGenre, LearnerText, AnnotationType, LearnerTextAnnotation
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Pupil)
+class PupilAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'education_level', 'school')
+    list_filter = ('school',)
+
+
+@admin.register(LearnerTextGenre)
+class LearnerTextGenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(LearnerText)
+class LearnerTextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'status', 'genre')
+    list_filter = ('genre',)
+
+
+@admin.register(AnnotationType)
+class AnnotationTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'key')
+    search_fields = ('name',)
+
+
+@admin.register(LearnerTextAnnotation)
+class LearnerTextAnnotationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'learner_text',
+        'begin_offset',
+        'end_offset',
+        'correct_text',
+        'comment',
+        'annotation_type',
+        'checker',
+    )
+    list_filter = ('annotation_type',)
--- server/apps/school_co/models.py
+++ server/apps/school_co/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class School(models.Model):
+    name = models.CharField(
+        max_length=128,
+        help_text="название школы в которой учится ученик"
+    )
+
+class Pupil(models.Model):
+    full_name = models.CharField(max_length=128, help_text="ФИО")
+    education_level = models.IntegerField(help_text="класс")
+    school = models.ForeignKey('School', on_delete=models.PROTECT)
+    # регион
+
+class LearnerTextGenre(models.Model):
+    title = models.CharField(max_length=128, help_text="человекочитаемое название жанра")
+
+class LearnerText(models.Model):
+    class Status(models.IntegerChoices):
+        NEW = 1
+        CHECKED = 10
+
+    text = models.TextField()
+    # скан (будет добавлено как позже как ссылка на модель файлов)
+    status = models.IntegerField(choices=Status.choices)
+    genre = models.ForeignKey('LearnerTextGenre', on_delete=models.PROTECT)
+
+class AnnotationType(models.Model):
+    name = models.CharField(max_length=64, help_text="человекочитаемое название")
+    key = models.CharField(
+        max_length=64,
+        help_text="ключи являются materialized path и позволяют делать из типов аннотаций иерархичную структуру")
+
+class LearnerTextAnnotation(models.Model):
+    learner_text = models.ForeignKey('LearnerText', on_delete=models.PROTECT)
+    begin_offset = models.PositiveSmallIntegerField(
+        help_text="offset начала аннотации")
+    end_offset = models.PositiveSmallIntegerField(
+        help_text="offset окончания аннотации")
+
+    correct_text = models.TextField(
+        blank=True,
+        help_text="иcправление аннотации (текст с правильный вариантом. Если в изначальном тексте подстроку [offset_begin:offset_end] заменить на исправление, то должен получиться корректный текст)")
+
+    comment = models.TextField(blank=True, default="")
+    annotation_type = models.ForeignKey('AnnotationType', on_delete=models.PROTECT)
+    checker = models.ForeignKey(User, on_delete=models.PROTECT)
